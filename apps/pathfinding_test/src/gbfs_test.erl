602cc10426d1de454fa30d995c3360bfa055e99e
dong50252409 <304887750@qq.com>
2024-08-08 11:56:08 +0800

    优化流程

---
+++ apps/pathfinding_test/src/gbfs_test.erl
@@ -0,0 +1,20 @@
+-module(gbfs_test).
+
+%% API
+-export([test/1]).
+
+test(Options) ->
+    fun(Maze) ->
+        Width = size(element(1, Maze)),
+        High = size(Maze),
+        ValidFun =
+            fun({X, Y}) ->
+                X > 0 andalso X =< Width andalso Y > 0 andalso Y =< High andalso element(X, element(Y, Maze)) =/= 0
+            end,
+        case gbfs:search({1, 1}, {Width, High}, ValidFun, Options) of
+            {max, FullPath} ->
+                FullPath;
+            _Other ->
+                []
+        end
+    end.
